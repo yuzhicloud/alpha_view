f03649ce9e6ea6360adc9a2291dc7a9aff9bfa2b
billyhotjava <billy.xiezhimin@gmail.com>
2018-09-25 17:46:33 +0800

    update to ng2-admin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,6 @@ import { AuthGuard } from './shared';
 
 // AoT requires an exported function for factories
 export const createTranslateLoader = (http: HttpClient) => {
-    /* for development
-    return new TranslateHttpLoader(
-        http,
-        '/start-angular/SB-Admin-BS4-Angular-6/master/dist/assets/i18n/',
-        '.json'
-    ); */
     return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 };
 
